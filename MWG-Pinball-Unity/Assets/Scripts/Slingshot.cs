835d14ebfdb2d08a5a6a066a982e19fffa6a26a8
Justin Ryder <justin@justin-ryder.com>
2014-12-17 10:52:44 -0500

    added slingshot script and prefab.

---
+++ MWG-Pinball-Unity/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour
+{
+  public float Force = 100;
+
+  public string BallTag = "Ball";
+
+  private Vector3 ForceDirection
+  {
+    get { return transform.forward; }
+  }
+
+  public void OnTriggerEnter(Collider collider)
+  {
+    if (collider.gameObject.tag == BallTag)
+    {
+      collider.gameObject.rigidbody.AddForce(ForceDirection * Force);
+    }
+  }
+
+  public void OnDrawGizmos()
+  {
+    Gizmos.DrawLine(transform.position, transform.position + (ForceDirection * .2f));
+  }
+}
