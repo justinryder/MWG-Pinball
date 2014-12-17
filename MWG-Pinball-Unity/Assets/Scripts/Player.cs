e3d3586d39e49cf2b8f8fa3ffe0f3f85b2799b87
Justin Ryder <justin@justin-ryder.com>
2014-12-17 12:04:07 -0500

    added turn controller and player.

---
+++ MWG-Pinball-Unity/Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿public class Player
+{
+  public Player(int number)
+  {
+    Number = number;
+  }
+
+  public int Number { get; private set; }
+
+  public int Score { get; private set; }
+
+  public void AddScore(int points)
+  {
+    Score += points;
+  }
+}
