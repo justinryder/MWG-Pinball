484cfc9df7d7bf7cd368da0b9b1c48ec152da35b
FordyceGarfield <fordyce.garfield@gmail.com>
2014-12-17 14:35:12 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	MWG-Pinball-Unity/Assembly-CSharp-vs.csproj
    	MWG-Pinball-Unity/Assembly-CSharp.csproj
    	MWG-Pinball-Unity/MWG-Pinball-Unity-csharp.sln
    	MWG-Pinball-Unity/MWG-Pinball-Unity-csharp.v12.suo
    	MWG-Pinball-Unity/MWG-Pinball-Unity.sln

--- MWG-Pinball-Unity/Assets/Scripts/PlungerController.cs
+++ MWG-Pinball-Unity/Assets/Scripts/PlungerController.cs
@@ -13,8 +13,12 @@ public class PlungerController : MonoBehaviour
   // Update is called once per frame
   private void Update()
   {
-      
       Vector3 input = new Vector3(0, Input.GetAxis("Vertical"), 0);
       transform.position += input * Time.deltaTime;
+  }
+
+  public void SpawnBall()
+  {
+      //TODO: Spawn that bad boy!
   }
 }
