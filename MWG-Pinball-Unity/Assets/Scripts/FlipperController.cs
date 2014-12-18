7f42cdf47df5bb1e5f788d191450cc6556b7501f
unknown <ipercoco@ipercoco4.internal.mywebgrocer.com>
2014-12-18 10:45:45 -0500

    added points to drop targets and rolloverl ights. added ball saucer

--- MWG-Pinball-Unity/Assets/Scripts/FlipperController.cs
+++ MWG-Pinball-Unity/Assets/Scripts/FlipperController.cs
@@ -25,6 +25,8 @@ namespace Assets.Scripts
     public float FlipperStrength = 100f;
     public float FlipperDamper = 1f;
 
+    public JointSpring spring = new JointSpring();
+
     void Awake()
     {
       hingeJoint.useSpring = true;
@@ -33,14 +35,13 @@ namespace Assets.Scripts
     // Update is called once per frame
     private void Update()
     {
-      JointSpring spring = new JointSpring();
-      this.transform.localScale = new Vector3(10,10,10);
-
+     
       spring.spring = this.FlipperStrength;
       spring.damper = this.FlipperDamper;
+
       if (FlipperSide == FlipperSide.Left)
       {
-        if (this._flipDownLeft)
+        if (_flipDownLeft)
         {
           spring.targetPosition = pressedPositionLeft;
         }
@@ -51,17 +52,18 @@ namespace Assets.Scripts
 
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-          this._flipDownLeft = true;
+          _flipDownLeft = true;
         }
+
         if (Input.GetKeyUp(KeyCode.LeftControl))
         {
-          this._flipDownLeft = false;
+          _flipDownLeft = false;
         }
       }
 
       if (FlipperSide == FlipperSide.Right)
       {
-        if (this._flipDownRight)
+        if (_flipDownRight)
         {
           spring.targetPosition = pressedPositionRight;
         }
@@ -72,11 +74,12 @@ namespace Assets.Scripts
 
         if (Input.GetKeyDown(KeyCode.RightControl))
         {
-          this._flipDownRight = true;
+          _flipDownRight = true;
         }
+
         if (Input.GetKeyUp(KeyCode.RightControl))
         {
-          this._flipDownRight = false;
+          _flipDownRight = false;
         }
       }
 
