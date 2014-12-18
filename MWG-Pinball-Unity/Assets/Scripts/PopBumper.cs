d7eace6b2ba8797a5d09a2abb56a97f8f0c1ea2c
Justin Ryder <justin@justin-ryder.com>
2014-12-18 17:45:13 -0500

    added lights to pop bumpers.

--- MWG-Pinball-Unity/Assets/Scripts/PopBumper.cs
+++ MWG-Pinball-Unity/Assets/Scripts/PopBumper.cs
@@ -4,15 +4,36 @@ public class PopBumper : MonoBehaviour
 {
   public TurnController TurnController;
 
+  public GameObject Light;
+
   public float Force = 100;
 
   public string BallTag = "Ball";
+
+  public float FlashDuration = 0.5f;
+
+  private float _lastHitTime;
 
   public void Start()
   {
     if (TurnController == null)
     {
       Debug.LogError("Assign the TurnController to the PopBumper!");
+    }
+
+    if (Light == null)
+    {
+      Debug.LogError("Assign the Light to the PopBumper!");
+    }
+
+    Light.SetActive(false);
+  }
+
+  public void Update()
+  {
+    if (Time.time > _lastHitTime + FlashDuration)
+    {
+      Light.SetActive(false);
     }
   }
 
@@ -24,6 +45,17 @@ public class PopBumper : MonoBehaviour
       collision.gameObject.rigidbody.AddForce(directionToBall * Force);
 
       TurnController.CurrentPlayer.AddScore(100);
+
+      Light.SetActive(true);
+      _lastHitTime = Time.time;
     }
   }
+
+  //public void OnCollisionExit(Collision collision)
+  //{
+  //  if (collision.gameObject.tag == BallTag)
+  //  {
+  //    _lastHitTime = Time.time;
+  //  }
+  //}
 }
