cd51e907aa5f08c20f9a3e8948f12cf60596e46e
Justin Ryder <justin@justin-ryder.com>
2014-12-18 11:30:53 -0500

    added gates.

---
+++ MWG-Pinball-Unity/Assets/Scripts/Gate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+  public TurnController TurnController;
+
+  public string BallTag = "Ball";
+
+  public void Start()
+  {
+    if (TurnController != null)
+    {
+      Debug.LogError("Assign turnController to Gate!");
+    }
+
+    TurnController.OnTurnStart += TurnControllerOnOnTurnStart;
+  }
+
+  public void Destroy()
+  {
+    TurnController.OnTurnStart -= TurnControllerOnOnTurnStart;
+  }
+
+  public void OnTriggerExit()
+  {
+    collider.isTrigger = false;
+  }
+
+  private void TurnControllerOnOnTurnStart(object sender, OnTurnStartEventArgs onTurnStartEventArgs)
+  {
+    collider.isTrigger = true;
+  }
+}
